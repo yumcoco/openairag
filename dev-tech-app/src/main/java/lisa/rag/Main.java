9edf5605025702b59721fa490cabfccfd3073e84
yummy30 <pkulisha@gmail.com>
2025-04-13 15:47:07 +0200

    update

---
+++ dev-tech-app/src/main/java/lisa/rag/Main.java
@@ -0,0 +1,11 @@
+package lisa.rag;
+
+/**
+ * @author yummy
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ dev-tech-app/src/main/java/lisa/rag/dev/tech/app/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author yummy
+ * @version 1.0
+ */
+package lisa.rag.dev.tech.app;
